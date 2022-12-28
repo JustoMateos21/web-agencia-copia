f3dd04f2fd60430efca57edb9b30ea1e66dbf5c8
JustoMateos21 <98715129+JustoMateos21@users.noreply.github.com>
2022-12-28 16:31:32 -0300

    arreglo-en-nav-creacion-services

--- src/Screens/Services.js
+++ src/Screens/Services.js
@@ -1,7 +1,16 @@
 import React from "react";
+import styles from "./Services.module.css";
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div className={styles.services}>
+      <p>We solve your company's problems by creating amazing web pages.</p>
+      <img
+        className={styles.webImage}
+        src={require("../assets/webImage.jpg")}
+      />
+    </div>
+  );
 };
 
 export default Services;
