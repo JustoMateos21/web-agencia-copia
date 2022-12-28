4a8a954ad9a8d912279276e6018ad8b7bdf2883e
JustoMateos21 <98715129+JustoMateos21@users.noreply.github.com>
2022-12-28 17:18:01 -0300

    HowWeWork-seccion-terminada

--- src/Screens/Benefits.js
+++ src/Screens/Benefits.js
@@ -1,7 +1,20 @@
 import React from "react";
+import styles from "./Benefits.module.css";
+import { BsFillDoorOpenFill } from "react-icons/bs";
+import { FaPeopleCarry } from "react-icons/fa";
+import { FaMoneyBillAlt } from "react-icons/fa";
+import { AiOutlineSchedule } from "react-icons/ai";
+import { AiOutlineRise } from "react-icons/ai";
+import { FiRepeat } from "react-icons/fi";
 
 const Benefits = () => {
-  return <div>Benefits</div>;
+
+  return (
+    <div className={styles.Benefits}>
+      <p>Benefits of having a professional web</p>
+      <p
+    </div>
+  );
 };
 
 export default Benefits;
